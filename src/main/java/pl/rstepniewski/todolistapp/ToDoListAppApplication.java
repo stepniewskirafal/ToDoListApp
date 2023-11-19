e483411475c7296dde68758a126f71ebf1f80197
Rafal Stepniewski <vultur12@gmail.com>
2023-11-19 15:38:40 +0100

    project dependencies and src catalog

---
+++ src/main/java/pl/rstepniewski/todolistapp/ToDoListAppApplication.java
@@ -0,0 +1,13 @@
+package pl.rstepniewski.todolistapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListAppApplication.class, args);
+	}
+
+}
