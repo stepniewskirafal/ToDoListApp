044316a436e96a7c31de70b8e04e8a367cd479ee
Rafal Stepniewski <vultur12@gmail.com>
2023-11-22 20:13:47 +0100

    New tasks

--- src/main/java/pl/rstepniewski/todolistapp/config/TodoItemDataLoader.java
+++ src/main/java/pl/rstepniewski/todolistapp/config/TodoItemDataLoader.java
@@ -23,11 +23,15 @@ public class TodoItemDataLoader implements CommandLineRunner {
 
     private void loadSeedData() {
         if (todoItemRepository.count() == 0) {
-            TodoItem todoItem1 = new TodoItem("get the milk");
-            TodoItem todoItem2 = new TodoItem("rake the leaves");
+            TodoItem todoItem1 = new TodoItem("Write an application in Spring Boot.");
+            todoItem1.setComplete(true);
+            TodoItem todoItem2 = new TodoItem("Do a running workout.");
+            TodoItem todoItem3 = new TodoItem("Stretch by practicing yoga.");
+
 
             todoItemRepository.save(todoItem1);
             todoItemRepository.save(todoItem2);
+            todoItemRepository.save(todoItem3);
         }
 
         logger.info("Number of TodoItems: {}", todoItemRepository.count());
